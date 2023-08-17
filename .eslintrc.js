4f7292941d413253fdc9255d06202c2b1f62751a
Ronald A. Richardson <me@ron.dev>
2023-08-17 21:46:25 +0800

    WIP / upgraded dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,7 +29,7 @@ module.exports = {
         'node/no-unpublished-require': [
             'error',
             {
-                allowModules: ['resolve'],
+                allowModules: ['resolve', 'broccoli-funnel'],
             },
         ],
     },
